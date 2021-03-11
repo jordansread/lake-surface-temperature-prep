774f0c2a79c61ea9fb00e941b91d4a91b7eb02bb
Jordan S Read <jread@usgs.gov>
2021-03-11 03:31:13 -0600

    removing three coastal lakes on small islands, no NLDAS

--- 4_drivers_fetch/src/metadata_utils.R
+++ 4_drivers_fetch/src/metadata_utils.R
@@ -1,14 +1,14 @@
 
 
 create_metadata_file <- function(outfile, site_cell_indices, lakes_sf_ind, obs_ind_fl, elevation_ind_fl){
-
+  
   lakes_sf <- sc_retrieve(lakes_sf_ind) %>% readRDS()
-
+  
   obs_sites <- sc_retrieve(obs_ind_fl) %>% feather::read_feather() %>% pull(site_id) %>% unique()
-
-
+  
+  
   elevations <- sc_retrieve(elevation_ind_fl) %>% read_csv()
-
+  
   # add lat/lon
   lakes_sf %>% inner_join(site_cell_indices) %>%
     # observed: TRUE/FALSE
@@ -16,8 +16,8 @@ create_metadata_file <- function(outfile, site_cell_indices, lakes_sf_ind, obs_i
     # can't do coords on polygons, so convert to centroids
     st_centroid() %>% mutate(lon = {st_coordinates(Shape)[,1]}, lat = {st_coordinates(Shape)[,2]}) %>%
     st_drop_geometry() %>%
-    select(-Elevation) %>%
+    select(-Elevation, -FType, -FCode) %>%
     inner_join(elevations, by = 'site_id') %>%
     write_csv(outfile)
-
+  
 }
